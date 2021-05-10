f898fe267632f2bce707dd828345f37b446c1481
Ed <76992905+edphan@users.noreply.github.com>
2021-05-09 23:27:06 -0700

    add 100vh, and move the home-content -20% so it's in the middle a bit

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,17 +4,19 @@ import linkedin from '../images/icons/LinkedIn-Logos/LI-In-Bug.png';
 function Home() {
 	return (
 		<div className='home-container'>
-			<div className='home-text'>
-				<h1 className='home-gradient-text'>hello world, I'm Edward</h1>
-				<h2 className='home-description-text'>// full-stack engineer</h2>
-			</div>
-			<div className='home-logo-container'>
-				<a href='https://github.com/edphan'>
-					<img src={github} alt='github logo' />
-				</a>
-				<a href='https://www.linkedin.com/in/trungngocphan/'>
-					<img src={linkedin} alt='linkedin logo' />
-				</a>
+			<div className='home-content'>
+				<div className='home-text'>
+					<h1 className='home-gradient-text'>hello world, I'm Edward</h1>
+					<h2 className='home-description-text'>// full-stack engineer</h2>
+				</div>
+				<div className='home-logo-container'>
+					<a href='https://github.com/edphan'>
+						<img src={github} alt='github logo' />
+					</a>
+					<a href='https://www.linkedin.com/in/trungngocphan/'>
+						<img src={linkedin} alt='linkedin logo' />
+					</a>
+				</div>
 			</div>
 		</div>
 	);
