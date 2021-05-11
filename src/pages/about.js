5295a482596c1e0571d05bd4b9dec9d65bd675a3
Ed <76992905+edphan@users.noreply.github.com>
2021-05-10 19:07:26 -0700

    add about me info

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,20 @@ function About() {
 				<h2 className='about-title'>
 					<span>//</span> about
 				</h2>
+				<p>
+					I have always wanted to use programming to build something cool and amazing. I enrolled and received the Full-Stack Engineer degree from Codecademy in
+					2021. This program was intensive - it provided a boot camp-like experience. I rigorously studied and coded every day, which allowed me to graduated
+					before the estimated six months. I am highly self-motivated and a quick learner. When I'm not coding, I'd like to think and discuss business
+					opportunities or flying drones. I graduated from UC Berkeley with a BS degree in Energy Engineering in Spring 2021.
+				</p>
+
+				<p>
+					I am great at: <br></br>
+					Front-end: JavaScript ES6 | React and Redux | HTML5 | CSS3 (grid and flex box) | jQuery; <br></br>
+					Back-end: Node JS | Express | RESTful APIs;<br></br>
+					Database: PostgreSQL; <br></br>
+					Familiar with: Python, Jest, Chai/Mocha; <br></br>
+				</p>
 			</div>
 		</div>
 	);
